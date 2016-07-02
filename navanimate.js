5c8ca1d2535ed6f6e9760508b889e66f519892b1
Junior Alves <junioralves.webdeveloper@gmail.com>
2016-07-02 12:48:29 -0300

    Adicionei função
    Função de animação da "height"

--- navanimate.js
+++ navanimate.js
@@ -3,11 +3,27 @@ $(function(){
    $(window).scroll(function(){
        var topscroll = $(window).scrollTop();
        
-       if (scroll > 100) {
+       if (scroll > 587) {
            $('#navbar').removeClass("navbar-default").addClass("navbar-style");
        }
        else {
            $('#navbar').removeClass("navbar-style").addClass("navbar-default");
        }
    }); 
+});
+
+// Adicionando aumento e diminuição do "height"
+
+$(function(){
+    $(window).scroll(function(){
+        var top = document.getElementById('header');
+        var down = window.pageYOffset;
+        
+        if(down > 587) {
+            top.style.height = "70px";
+        }
+        else{
+            top.style.height = "150px";
+        }
+    });
 });
